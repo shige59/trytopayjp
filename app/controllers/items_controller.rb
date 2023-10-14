72ba06d9f872cfed093bacc576fdffb94fe00187
shige59 <141386004+shige59@users.noreply.github.com>
2023-10-14 13:56:19 +0900

    first commit

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,61 @@
+class ItemsController < ApplicationController
+  before_action :set_item, except: [:index, :new, :create]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :contributor_confirmation, only: [:edit, :update]
+
+  def index
+    @items = Item.all.order('created_at DESC')
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(params[:id])
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name,
+      :info,
+      :category_id,
+      :condition_id,
+      :price,
+      :postage_type_id,
+      :prefecture_id,
+      :shipping_time_id,
+      :image
+    ).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @item.user
+  end
+end
